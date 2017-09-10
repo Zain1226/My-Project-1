fdb14fde68cac6c5adb89dd7cb308f6ba1c4c3d0
Zain1226 <abbas.zain124@gmail.com>
2017-09-10 15:44:15 +0500

    new project

---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string UserName, String Password)
+        {
+            if (UserName == "Ali" && Password == "pass123")
+            {
+                string msg = "Welcome" + UserName;
+                return Content(msg);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+    
+}
---
+++ WebApplication9/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
